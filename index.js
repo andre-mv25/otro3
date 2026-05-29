08001dfb10ff466a153fa9a9be6128660f2cc644
andre miranda <lopezvargasandre@outlook.com>
2026-05-29 12:53:08 -0600

    Fix typos, add EJS template for planets

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'views/planetas')));
+app.set('view engine', 'ejs');
+
 app.get('/', (_req, res) => {
-    res.send('hola mundo');
+    res.render('index', { port: PORT });
 });
 
 // 404 handler
